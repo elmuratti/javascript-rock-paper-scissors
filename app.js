7ffc1373fae8d0f4b082cd29fb78ba474e3ccb80
Murat Menevse <murat@adana01.de>
2025-07-07 08:45:13 +0200

    challenge without counter count function
    actally works only one match. counting <= 1

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+// counter
+let counterHuman = 0;
+let counterComputer = 0;
+
+// Auswahl Computer
 const getComputerChoice = () => {
     const number = Math.random();
-    console.log(number)
     if (number <= 0.3333) {
         return 'rock';
     }  else if (number > 0.3333 && number <= 0.6666 ){
@@ -12,4 +16,32 @@ const getComputerChoice = () => {
 
 
 
-const getHumanChoice = () => prompt('Your turn: Rock, Paper or Scissor', '' );
+
+
+
+// Challenge
+const challenge = () => {
+
+// Auswahl Mensch 
+const getHumanChoice = prompt('your turn: rock, paper or scissors', '' );
+const computerChoice = getComputerChoice();
+console.log('human = ' + getHumanChoice);
+console.log('computer = ' + computerChoice); 
+console.log(typeof(getHumanChoice)); 
+console.log(typeof(computerChoice));
+    if (getHumanChoice === computerChoice){
+
+        return 'draw';
+    } else if ((getHumanChoice === 'scissors' && computerChoice === 'paper') || (getHumanChoice === 'rock' && computerChoice === 'scissors') || (getHumanChoice === 'paper' && computerChoice === 'rock')){
+        counterHuman++;
+        console.log('You win!')
+    } else {
+        counterComputer++
+        console.log('Computer wins!')
+    }
+    return 'Human Score ' + counterHuman + ' Computer Score '  + counterComputer;
+}
+
+console.log(challenge())
+console.log('Final - Human: ' + counterHuman + ', Computer: ' + counterComputer);
+
